dbfe56e8d1e522175c3daa7d2f7a8efa43a43549
lcaoi <83848814+lcaoi@users.noreply.github.com>
2021-05-28 17:50:33 +0800

    Modified code
    Done: read int from file, file is selected by users.

--- bubblesort/main.c
+++ bubblesort/main.c
@@ -1,5 +1,23 @@
+#define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
+#include <stdlib.h>
 
-void main() {
-	printf("Hello world\n");
+int main() {
+    char filename[100];
+	int numbers;
+    char file;
+
+	FILE* fp;
+    printf("Please enter the name of the file: \n");
+    scanf("%s", filename);
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Fail to open file.");
+        return;  // exit
+    }
+
+    while (fscanf(fp, "%d", &numbers) != EOF) {
+        printf("%d\n", numbers);
+    }
 }
