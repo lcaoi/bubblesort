b6d30002c0d6d92d28c853330725e4a575c0f510
lcaoi <83848814+lcaoi@users.noreply.github.com>
2021-06-14 10:59:32 +0800

    Malloc
    Done: malloc

--- bubblesort/main.c
+++ bubblesort/main.c
@@ -6,35 +6,41 @@ int main() {
     // read from file
     char filename[100];
     int numbers;
-    //char file[100];
-    int* file = (int*)malloc(10 * sizeof(int));   // what if 10 is not given?
 
     FILE* fp;
     printf("Please enter the name of the file: \n");
-    scanf("%s", filename);
-
+    scanf("%s", filename);    
+    
     fp = fopen(filename, "r");
     if (fp == NULL) {
         printf("Fail to open file.");
         return;  // exit
     }
 
+    //count the number of integers
+    int count = 0;
+    while (fscanf(fp, "%d", &numbers) !=EOF) {
+        count++;
+    }
+
+    //reset pointer after first loop
+    rewind(fp);
+
+    //generate array by using malloc
+    int* file = (int*)malloc(count * sizeof(int)); // file is a pointer
+
+    //read the file again to get the integers
     int i = 0;
     while (fscanf(fp, "%d", &numbers) != EOF) {
         file[i] = numbers;
         i++;
     }
 
-    // for testing
-    int size = sizeof file / sizeof file[0];
-    printf("\n%d", size);   // output = 1
-    int size2 = sizeof filename / sizeof filename[0];
-    printf("\n%d\n", size2);   // output = 1
+    fclose(fp);
 
     // bubble sorting
-    //int size = 10; // hard code
     int temp;
-    for (int tail = 9; tail > 0; tail--) {
+    for (int tail = count-1; tail > 0; tail--) {
         for (int j = 0; j < tail; j++) {
             if (file[j] > file[j + 1]) {
                 temp = file[j + 1];
@@ -44,15 +50,8 @@ int main() {
         }
     }
 
-    for (int k = 0; k < 10; k++) {
+    //print the result
+    for (int k = 0; k < count; k++) {
         printf("%d  ", file[k]);
     }
-
-    // for testing
-    int size3 = sizeof file / sizeof file[0];
-    printf("\n%d", size3);   // output = 1
-    int size4 = sizeof filename / sizeof filename[0];
-    printf("\n%d", size4);   // output = 1
-
-    fclose(fp);
 }
