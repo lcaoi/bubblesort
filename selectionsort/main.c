d92cc61f8840766d223cf2b96ec3760bc01b0d65
lcaoi <83848814+lcaoi@users.noreply.github.com>
2021-08-16 21:46:02 +0800

    Amendment

--- selectionsort/main.c
+++ selectionsort/main.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// TODO: make the array "char file[100] to malloc(?), i.e. dynamic array with no fixed size"
-// testing 
+// Done: read the first line of the file
+// Todo: 1. make it the size of the file 2. read the rest of the file 3. write the sorting part
+
 int main() {
     // read from file
-    //char filename[100];
-    int* filename = (int*)malloc(10 * sizeof(int));
+    char filename[100];
+    int size = 0;
     int numbers;
-    //char file[100];
-    int* file = (int*)malloc(10 * sizeof(int));   // what if 10 is not given?
 
     FILE* fp;
     printf("Please enter the name of the file: \n");
@@ -22,30 +21,26 @@ int main() {
         return;  // exit
     }
 
-    int i = 0;
+    fscanf(fp, "%d", &numbers);
+    size = numbers;
+    /*
     while (fscanf(fp, "%d", &numbers) != EOF) {
         file[i] = numbers;
         i++;
-    }
+    }*/
+    fclose(fp);
 
-    // bubble sorting
+    printf("%d", size);
+    
+    // selection sorting
     /*
     int size = sizeof file / sizeof file[0];
     printf("%d", size);
     */
-    int size = 10; // hard code
-    int temp;
-    for (int tail = 9; tail > 0; tail--) {
-        for (int j = 0; j < tail; j++) {
-            if (file[j] > file[j + 1]) {
-                temp = file[j + 1];
-                file[j + 1] = file[j];
-                file[j] = temp;
-            }
-        }
-    }
 
+
+    /*
     for (int k = 0; k < 10; k++) {
         printf("%d  ", file[k]);
-    }
+    }*/
 }
